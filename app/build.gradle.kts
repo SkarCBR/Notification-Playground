ca4465d0ca60e9150edde39afe397b12f084a916
Oscar Olivella <oolivella@gmail.com>
2021-11-15 13:19:25 +0100

    update gradle dependencies versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,22 +54,22 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.compose.ui:ui:1.0.1")
-    implementation("androidx.compose.material:material:1.0.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.0.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.4.0-rc01")
+    implementation("androidx.compose.ui:ui:1.0.5")
+    implementation("androidx.compose.material:material:1.0.5")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.0.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
     api(platform("com.google.firebase:firebase-bom:27.0.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
 
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.0.4")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.0.5")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.0.5")
     implementation("io.coil-kt:coil:1.4.0")
 
 }
